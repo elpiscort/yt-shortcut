5b1d7f949113a01dd0f5c0a787f8583c96900fce
elpiscort <vdsc0rt@gmail.com>
2026-07-12 18:53:57 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/ytshortcut/MainActivity.java
+++ app/src/main/java/com/example/ytshortcut/MainActivity.java
@@ -10,11 +10,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
-            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com/"));
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com/?app=m&persist_app=1"));
             i.setPackage("com.opera.browser");
             startActivity(i);
         } catch (Exception e) {
-            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com/"));
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.youtube.com/?app=m&persist_app=1"));
             startActivity(i);
         }
         finish();
