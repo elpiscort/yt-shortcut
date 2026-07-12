09779a493806c561977140f94ddde11e9e80d57b
elpiscort <vdsc0rt@gmail.com>
2026-07-12 17:43:51 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/example/ytshortcut/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ytshortcut;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com"));
+            i.setPackage("com.opera.browser");
+            startActivity(i);
+        } catch (Exception e) {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com"));
+            startActivity(i);
+        }
+        finish();
+    }
+}
